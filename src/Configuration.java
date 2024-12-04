45ad712e4285aea22c5c8a7874f10c57d94069ff
S.H. Peduruhewa <sachinipeduruhewa@gmail.com>
2024-12-04 11:39:52 +0530

    Add promptForConfiguration method in the Configuration.java class

--- src/Configuration.java
+++ src/Configuration.java
@@ -1,4 +1,8 @@
-public class Configuration {
+import java.io.Serializable;
+import java.util.Scanner;
+
+public class Configuration implements Serializable {
+    private static final long serialVersionUID = 1L;
     private int totalTickets;
     private int ticketReleaseRate;
     private int customerRetrievalRate;
@@ -9,5 +13,18 @@ public class Configuration {
         this.ticketReleaseRate = ticketReleaseRate;
         this.customerRetrievalRate = customerRetrievalRate;
         this.maxTicketCapacity = maxTicketCapacity;
+    }
+
+    public static Configuration promptForConfiguration() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter total number of tickets: ");
+        int totalTickets = input.nextInt();
+        System.out.println("Enter ticket release rate: ");
+        int ticketReleaseRate = input.nextInt();
+        System.out.println("Enter customer retrieval rate: ");
+        int customerRetrievalRate = input.nextInt();
+        System.out.println("Enter maximum ticket capacity: ");
+        int maxTicketCapacity = input.nextInt();
+        return new Configuration(totalTickets, ticketReleaseRate, customerRetrievalRate, maxTicketCapacity);
     }
 }
