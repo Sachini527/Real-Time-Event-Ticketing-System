c11da67c017e8004a6a8b98c44200b12eaca695b
S.H. Peduruhewa <sachinipeduruhewa@gmail.com>
2024-12-04 19:56:58 +0530

    Create a Configuration object and call the methods in the Configuration class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-
+        try{
+            Configuration config = Configuration.promptForConfiguration();
+            config.saveToTextFile("config.txt");
+            System.out.println("Configuration saved successfully.");
+        } catch (IOException e) {
+            System.out.println("Error saving configuration to file: " + e.getMessage());
+        }
     }
 }
