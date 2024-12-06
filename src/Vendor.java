6fe08b6889e1eaec800cd8b1547d36242ffbcfc1
S.H. Peduruhewa <sachinipeduruhewa@gmail.com>
2024-12-06 23:11:29 +0530

    Add run method in the Vendor class.

--- src/Vendor.java
+++ src/Vendor.java
@@ -1,15 +1,35 @@
+import javax.swing.*;
+
 public class Vendor implements Runnable{
     private String vendorId;
     private int ticketsPerRelease;
     private int releaseInterval;
+    // create a TicketPool object
+    private TicketPool ticketPool;
+    private boolean isRunning;
 
-    public Vendor(String vendorId, int ticketsPerRelease, int releaseInterval) {
+    public Vendor(String vendorId, int ticketsPerRelease, int releaseInterval, TicketPool ticketPool) {
         this.vendorId = vendorId;
         this.ticketsPerRelease = ticketsPerRelease;
         this.releaseInterval = releaseInterval;
+        this.ticketPool = ticketPool;
+        this.isRunning = true;
     }
 
+    public void run(){
+        while(isRunning){
+            try{
+                Thread.sleep(releaseInterval);
+                ticketPool.addTickets(ticketsPerRelease);
+                System.out.println("Vendor " + vendorId + " added " + ticketsPerRelease + " tickets to the pool. Total tickets: " + ticketPool.getTicketCount());
+            } catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+                System.out.println("Vendor " + vendorId + " was interrupted.");
+            }
+        }
+    }
 
-
-
+    public void stop(){
+        isRunning = false;
+    }
 }
