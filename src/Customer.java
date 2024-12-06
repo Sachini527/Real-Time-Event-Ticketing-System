a92c0c775c5faea7bbae5c44828d147fb9e694a9
S.H. Peduruhewa <sachinipeduruhewa@gmail.com>
2024-12-06 23:19:08 +0530

    Add run method in the Customer class.

--- src/Customer.java
+++ src/Customer.java
@@ -1,10 +1,35 @@
 public class Customer implements Runnable{
     private String customerId;
     private int retrievalInterval;
+    private TicketPool ticketPool;
+    private boolean isRunning;
 
-    public Customer(String customerId, int retrievalInterval) {
+    public Customer(String customerId, int retrievalInterval, TicketPool ticketPool) {
         this.customerId = customerId;
         this.retrievalInterval = retrievalInterval;
+        this.ticketPool = ticketPool;
+        this.isRunning = true;
     }
 
+    @Override
+    public void run() {
+        while (isRunning) {
+            try {
+                Thread.sleep(retrievalInterval);
+                boolean success = ticketPool.removeTicket();
+                if (success) {
+                    System.out.println("Customer " + customerId + " purchased a ticket. Remaining tickets: " + ticketPool.getTicketCount());
+                } else {
+                    System.out.println("Customer " + customerId + " failed to purchase a ticket. No tickets available.");
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.err.println("Customer " + customerId + " was interrupted.");
+            }
+        }
+    }
+
+    public void stop() {
+        isRunning = false;
+    }
 }
