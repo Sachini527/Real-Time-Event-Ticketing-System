987ed9c397a4eee3c95c0ce28a4cc9adf1b0d9b6
S.H. Peduruhewa <sachinipeduruhewa@gmail.com>
2024-12-06 12:23:33 +0530

    Define attributes and put a constructor in the Customer class.

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,10 @@
-public class Customer {
+public class Customer implements Runnable{
+    private String customerId;
+    private int retrievalInterval;
+
+    public Customer(String customerId, int retrievalInterval) {
+        this.customerId = customerId;
+        this.retrievalInterval = retrievalInterval;
+    }
+
 }
